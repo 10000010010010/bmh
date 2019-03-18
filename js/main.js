f8e194fa48df7a6ded2767f7db86024e79634827
Hrimthursum <jager81@tuta.io>
2019-03-18 02:24:03 +0100

    refined menu, added logo toggle

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const toggle = document.querySelector(".menu-icon");
 const menu = document.querySelector(".navmenu");
+const logoimg = document.querySelector(".logo-img");
 
 toggle.addEventListener("click", function toggleMenu() {
   console.log("Clicked");
   menu.classList.toggle("toggleMenu");
+  logoimg.classList.toggle("toggleImg");
 });
