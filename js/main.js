298aef27402e04bde1537f1f5a4eb19cd29a0452
Hrimthursum <jager81@tuta.io>
2019-04-15 00:18:41 +0200

    finished menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 const toggle = document.querySelector(".menu-icon");
 const menu = document.querySelector(".navmenu");
 const logoimg = document.querySelector(".logo-img");
+const menuLinks = document.querySelector(".navmenu__items");
 
 toggle.addEventListener("click", function toggleMenu() {
   console.log("Clicked");
   menu.classList.toggle("toggleMenu");
   logoimg.classList.toggle("toggleImg");
+});
+
+menuLinks.addEventListener("click", function toggleMenu() {
+  console.log("Clicked");
+  menu.classList.toggle("toggleMenu");
+  logoimg.classList.toggle("toggleImg");
+});
+
+window.addEventListener("scroll", function scrollIcon() {
+  toggle.classList.toggle("toggleMenu");
 });
